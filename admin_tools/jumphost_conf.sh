2de3197f8a0add172190e9645e490480967b3d00
zabenno <benc853@gmail.com>
2021-02-25 11:38:20 +1300

    Added dependecy file for kubectl & eksctl.

---
+++ admin_tools/jumphost_conf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+#Install kubectl v1.19.6
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/
+
+#Install IAM authenticator
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mv ./aws-iam-authenticator /usr/local/bin/
