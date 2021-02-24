96d34225838fdc057bf5f330579b3f06f8d3a4c8
zabenno <benc853@gmail.com>
2021-02-25 00:59:26 +1300

    Added awscli to install script.

--- admin_tools/jumphost_conf.sh
+++ admin_tools/jumphost_conf.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Install awscli
+sudo apt install awscli -y
+
 #Install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
